81376531b76e671ffb1d24bfe273fdda3d866d21
Sam Jacob <sam21nw@gmail.com>
2022-06-07 19:23:11 +0400

    updated

---
+++ TeklaReportsWpfApp_T2016/Views/SplashScreenWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace TeklaReportsApp
+{
+  /// <summary>
+  /// Interaction logic for SplashScreenWindow.xaml
+  /// </summary>
+  public partial class SplashScreenWindow : Window
+  {
+    public SplashScreenWindow()
+    {
+      InitializeComponent();
+    }
+    public double Progress
+    {
+      get { return progressBar.Value; }
+      set { progressBar.Value = value; }
+    }
+  }
+}
